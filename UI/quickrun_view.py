67aaefa6c320ce9a44408e12735c0e7eb8237c6f
Louise McCann <louise.mccann@stfc.ac.uk>
2017-04-25 10:41:38 +0100

    add qwidget class for tabs

--- UI/quickrun_view.py
+++ UI/quickrun_view.py
@@ -1,14 +1,22 @@
 from PyQt4 import uic, QtCore
-from PyQt4.QtGui import QMainWindow, QApplication
+from PyQt4.QtGui import QMainWindow, QApplication, QWidget
 import sys
+
+class ScanTab(QWidget):
+    
+    def __init__(self):
+        super(ScanTab,self).__init__()
+        self.energytab = uic.loadUi('./EnergyWindowScan.ui',self)
 
 class MainWindowView(QMainWindow):
 
     def __init__(self):
         super(MainWindowView,self).__init__()
-        uic.loadUi('./IndirectQuickRun.ui', self)
+        self.ui = uic.loadUi('./IndirectQuickRun.ui', self)
         self.setWindowTitle("Indirect QuickRun")
-        
+        self.tabs = ScanTab()
+        self.ui.tb_quickrun.addTab(self.tabs.energytab, "Energy Window Scan")
+
 qApp = QApplication(sys.argv)
 aw = MainWindowView()
 aw.show()
