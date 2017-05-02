c62cee63905e784b2cb25866c8ad9fc2ae75cb72
Louise McCann <louise.mccann@stfc.ac.uk>
2017-05-02 09:35:20 +0100

    add presenter

---
+++ UI/quickrun_presenter.py
@@ -0,0 +1,13 @@
+
+class QuickRunPresenter(object):
+
+    def __init__(self, view):
+        self._view = view
+
+    def onPlotClicked(self):
+        view = self._view
+
+        plot_option = view.getPlotOption()
+        print(plot_option)
+
+        return None
